b894485ee8e82ae690a1579b9ff42801dcb65b03
Raphael Goulais <raphael.goulais@f5c.fr>
2023-12-11 14:19:42 +0100

    feat: Day 10

---
+++ src/bin/11.rs
@@ -0,0 +1,76 @@
+advent_of_code::solution!(11);
+
+use std::collections::BTreeMap;
+
+fn part_all(input_str: &str, galaxy_age: usize) -> Option<usize> {
+    let mut effective_row = 0;
+    let mut last_row = 0;
+    let mut coordinates_to_value = BTreeMap::new();
+
+    for (i, row) in input_str.lines().enumerate() {
+        for (j, item) in row.chars().enumerate() {
+            if item == '#' {
+                let ecart_row = i - last_row;
+                last_row = i;
+                if ecart_row > 1 {
+                    effective_row += 1 + (galaxy_age * (ecart_row - 1));
+                } else {
+                    effective_row += ecart_row;
+                }
+                coordinates_to_value.insert((i, j), effective_row);
+            }
+        }
+    }
+    let mut sorted_coordinates = coordinates_to_value.iter().collect::<Vec<_>>();
+    sorted_coordinates.sort_by_key(|&(coordinate, _)| coordinate.1);
+
+    let (mut effective_col, mut last_col) = (0usize, 0usize);
+    let (mut coords, mut total) = (vec![], 0usize);
+
+    for &(coord, &row) in sorted_coordinates.iter() {
+        let mut ecart_col = coord.1 - last_col;
+        last_col = coord.1;
+        if ecart_col > 1 {
+            ecart_col = 1 + (galaxy_age * (ecart_col - 1));
+        }
+        effective_col += ecart_col;
+        let new_coord = (row, effective_col);
+        for &(r, c) in coords.iter() {
+            total += new_coord.0.abs_diff(r) + new_coord.1.abs_diff(c)
+        }
+        coords.push(new_coord);
+    }
+    Some(total)
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    return part_all(input, 2);
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    return part_all(input, 1000000);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_one() {
+        let result = part_all(&advent_of_code::template::read_file("examples", DAY), 2);
+        assert_eq!(result, 374);
+    }
+
+    #[test]
+    fn test_two() {
+        let result = part_all(&advent_of_code::template::read_file("examples", DAY), 10);
+        assert_eq!(result, 1030);
+    }
+
+    #[test]
+    fn test_three() {
+        let result = part_all(&advent_of_code::template::read_file("examples", DAY), 100);
+        assert_eq!(result, 8410);
+    }
+}
